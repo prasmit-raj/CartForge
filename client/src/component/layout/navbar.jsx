25241272dcab5b17bc7fda99ed7e4eb5413d0734
Prasmit Raj <prasmitraj056@gmail.com>
2026-08-04 17:12:58 +0530

    added nabar

---
+++ client/src/component/layout/navbar.jsx
@@ -0,0 +1,15 @@
+function Nav(){
+return (
+<header className="flex items-center justify-between px-8 py-4 shadow-md bg-white">
+<h1>CartForge</h1>
+
+<nav className="nav-links">
+    <a href="#">Home</a>
+    <a href="#">Login</a>
+    <a href="#">Signup</a>
+</nav>
+
+</header>
+);
+}
+export default Nav;
---
+++ client/src/pages/home/home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../component/layout/navbar.jsx";
+
+function Home(){
+    return (
+        <>
+        <Navbar />
+
+        </>
+    );
+}
+export default Home;
