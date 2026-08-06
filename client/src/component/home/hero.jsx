d27251f969f0b767f0aa49e8cd1d4f8c0df40fb8
Prasmit Raj <prasmitraj056@gmail.com>
2026-08-06 12:54:05 +0530

    improved hero section

--- client/src/component/home/hero.jsx
+++ client/src/component/home/hero.jsx
@@ -1,16 +1,27 @@
 import hero from "../../assets/hero.png";
 function Hero(){
 return (
-    <article className="flex ">
+ <article   className="flex items-center justify-between mx-8 my-8 rounded-2xl gap-8 border-0 h-96 px-8 py-4 shadow-md bg-slate-100 text-slate-900">
 
-        <div>
-             <h1>Welcome to CartForge</h1>
-            <p>Discover the ultimate shopping experience with CartForge, your one-stop destination for all your needs. Explore a wide range of products, enjoy seamless navigation, and find the best deals tailored just for you.</p>
-        <p>Start your shopping journey today and experience the convenience of CartForge!</p>
-        <a href="#"> <button>Get Started</button> </a>  
+        <div className="flex flex-col gap-4 justify-center items-start w-1/2">
+        <h1 className="text-5xl font-bold leading-tight">
+  Shop Smarter with CartForge
+</h1>
+
+<p className="text-lg text-slate-600 leading-8">
+  Discover thousands of premium products at unbeatable prices. From everyday
+  essentials to the latest trends, CartForge brings you quality, convenience,
+  and secure shopping—all in one place.
+</p>
+
+<a href="#">
+  <button className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-red-700 transition duration-300" >
+    Shop Now
+  </button>
+</a>
         </div>
        
-      <img src={hero} alt="CartForge Hero"  height="200" />
+      <img src={hero} alt="CartForge Hero"  className="h-80 w-auto" />
       
       
       
