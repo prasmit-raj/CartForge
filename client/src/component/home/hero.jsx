9091cb4f100ad125612eebee0bd4ca3fe360e4f3
Prasmit Raj <prasmitraj056@gmail.com>
2026-08-06 12:30:28 +0530

    update navbar

--- client/src/component/home/hero.jsx
+++ client/src/component/home/hero.jsx
@@ -1,11 +1,17 @@
+import hero from "../../assets/hero.png";
 function Hero(){
 return (
-    <article className="border p-8 bg-gray-100 rounded-lg shadow-md text-center">
-        <b>  <h1>Welcome to CartForge</h1></b>
-      
-        <p>Discover the ultimate shopping experience with CartForge, your one-stop destination for all your needs. Explore a wide range of products, enjoy seamless navigation, and find the best deals tailored just for you.</p>
+    <article className="flex ">
+
+        <div>
+             <h1>Welcome to CartForge</h1>
+            <p>Discover the ultimate shopping experience with CartForge, your one-stop destination for all your needs. Explore a wide range of products, enjoy seamless navigation, and find the best deals tailored just for you.</p>
         <p>Start your shopping journey today and experience the convenience of CartForge!</p>
-        <a href="#"> <button>Get Stared</button> </a>
+        <a href="#"> <button>Get Started</button> </a>  
+        </div>
+       
+      <img src={hero} alt="CartForge Hero"  height="200" />
+      
       
       
 
--- client/src/component/layout/navbar.jsx
+++ client/src/component/layout/navbar.jsx
@@ -1,12 +1,13 @@
 function Nav(){
 return (
-<header className="flex items-center justify-between px-8 py-4 shadow-md bg-black text-white">
-<h1>CartForge</h1>
+<header className="flex items-center justify-between px-8 py-6 h-10 shadow-md bg-slate-900 text-slate-100 shadow-md">
 
-<nav className=" flex space-x-4 cursor-pointer  ">
-    <a href="#" className="hover:text-red-500 ">Home</a>
-    <a href="#" className="hover:text-red-500">Login</a>
-    <a href="#" className="hover:text-red-500">Signup</a>
+<h1 className="text-2xl font-bold">CartForge</h1>
+
+<nav className=" flex space-x-4 cursor-pointer gap-12 justify-around px-4 py-4">
+    <a href="#" className="hover:text-sky-400  hover:scale-110 transition-all duration-200 hover:underline underline-offset-4">Home</a>
+    <a href="#" className="hover:text-sky-400  hover:scale-110 transition-all duration-200 hover:underline underline-offset-4">Login</a>
+    <a href="#" className="hover:text-sky-400  hover:scale-110 transition-all duration-200 hover:underline underline-offset-4">Signup</a>
 </nav>
 
 </header>
