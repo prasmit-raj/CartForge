c5b0015842919adc3d7260a051ebb0dfb20ba997
Prasmit Raj <prasmitraj056@gmail.com>
2026-08-08 22:16:10 +0530

    refactor: move routes out of App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/home/home";
-import Login from "./pages/login/login";
-import Signup from "./pages/signup/signup";
-import ForgetPassword from "./pages/forgetpassword/forgetpassword";
-import Enterotp from "./pages/forgetpassword/enterotp";
-import ResetPassword from "./pages/forgetpassword/resetpassword";
-import Loginotp from "./pages/login/loginotp";
-import Signupotp from "./pages/signup/signupotp";
-
-import Dashboard from "./pages/dashboard/dashboard";
+import AppRoute from "./route/approute";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/loginotp" element={<Loginotp />} />
-        <Route path="/signupotp" element={<Signupotp />} />
-
-        <Route path="/forgetpassword" element={<ForgetPassword />} />
-        <Route path="/enterotp" element={<Enterotp />} />
-        <Route path="/resetpassword" element={<ResetPassword />} />
-
-        <Route path="/signup" element={<Signup />} />
-
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <AppRoute />
   );
 }
 
 export default App;
+
+
+
